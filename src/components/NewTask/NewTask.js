a3322553e1e25716d0126c80a651e03a69d9001b
troy <hunter007nz@gmail.com>
2021-07-14 20:24:51 +1200

    changed to correct firebase api endpoint

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -16,7 +16,7 @@ const NewTask = (props) => {
   const enterTaskHandler = async (taskText) => {
     sendTaskRequest(
       {
-        url: 'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        url: 'https://react-http-1-f6f8f-default-rtdb.firebaseio.com/tasks.json',
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
